419f2fe09370d881b376248e246366e4438f580c
jaco0010 <jacovillegas12@gmail.com>
2026-05-05 10:59:36 -0500

    actualizacion clase 7-abril

--- paseoAPIgonzalez/src/main/java/com/example/paseoAPIgonzalez/modelos/Espacio.java
+++ paseoAPIgonzalez/src/main/java/com/example/paseoAPIgonzalez/modelos/Espacio.java
@@ -1,12 +1,63 @@
 package com.example.paseoAPIgonzalez.modelos;
 
+import jakarta.persistence.*;
 import java.util.UUID;
 
+@Entity
 public class Espacio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String nombre;
     private String descripcion;
     private String foto;
     private Integer aforo;
+
+
+    public Espacio() {
+    }
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Integer getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(Integer aforo) {
+        this.aforo = aforo;
+    }
 }
--- paseoAPIgonzalez/src/main/java/com/example/paseoAPIgonzalez/modelos/Reserva.java
+++ paseoAPIgonzalez/src/main/java/com/example/paseoAPIgonzalez/modelos/Reserva.java
@@ -1,12 +1,45 @@
 package com.example.paseoAPIgonzalez.modelos;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
 public class Reserva {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private LocalDateTime fecha;
     private Integer tiempo;
+
+
+    public Reserva() {
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(Integer tiempo) {
+        this.tiempo = tiempo;
+    }
 }
